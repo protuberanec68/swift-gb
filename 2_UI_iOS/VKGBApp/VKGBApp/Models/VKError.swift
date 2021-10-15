7a7aefa6612cb73375e39038c6a49fcdd701997c
igor2890 <igor2890@mail.ru>
2021-10-15 20:42:52 +0300

    delete unusing examples of code

--- 2_UI_iOS/VKGBApp/VKGBApp/Models/VKError.swift
+++ 2_UI_iOS/VKGBApp/VKGBApp/Models/VKError.swift
@@ -10,23 +10,3 @@ enum VKError: Error {
     case failResponseData
     case failJSONDecode
 }
-
-
-func foo() {
-    let x = 0
-    if x == 0 {
-        print("123")
-    } else {
-        print("error")
-    }
-}
-
-func bar(x: String) {
-    guard let y = Int(x) else {
-        print ("error")
-        return
-    }
-    print(y)
-    
-    let z = Int(x)
-}
