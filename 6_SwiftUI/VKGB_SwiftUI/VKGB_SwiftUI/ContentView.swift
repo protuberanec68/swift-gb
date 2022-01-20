44db6fa8cc455990f864f683e0e03104598b9bd5
igor2890 <igor2890@mail.ru>
2022-01-21 00:38:26 +0300

    init SwiftUI VK app

---
+++ 6_SwiftUI/VKGB_SwiftUI/VKGB_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VKGB_SwiftUI
+//
+//  Created by Игорь Андрианов on 21.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
