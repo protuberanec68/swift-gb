de28b76ce9e2f742db64d1e400cfc6248ebfea24
igor2890 <igor2890@mail.ru>
2022-01-30 19:29:45 +0300

    separated code, prepared navigationview

--- 6_SwiftUI/VKGB_SwiftUI/VKGB_SwiftUI/ContentView.swift
+++ 6_SwiftUI/VKGB_SwiftUI/VKGB_SwiftUI/ContentView.swift
@@ -2,147 +2,31 @@
 //  ContentView.swift
 //  VKGB_SwiftUI
 //
-//  Created by Игорь Андрианов on 21.01.2022.
+//  Created by Игорь Андрианов on 30.01.2022.
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var login = ""
-    @State private var password = ""
-    
+    @State private var presentMainScreen = false
     var body: some View {
-        GeometryReader{ geometry in
-            ZStack{
-                Image("back")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .edgesIgnoringSafeArea(.all)
-                    .frame(width: geometry.size.width, height: geometry.size.height, alignment: .center)
-                
-                ScrollView{
-                    VStack{
-                        Text("VKBGClient")
-                            .padding(.top, 50.0)
-                            .font(.largeTitle)
-                            .foregroundColor(.white)
-                        
-                        Text("Login")
-                            .padding(.top, 50.0)
-                            .font(.title2)
-                            .foregroundColor(.white)
-                        
-                        TextField(                            "Email address", text: $login)
-                            .textInputAutocapitalization(.never)
-                            .disableAutocorrection(true)
-                            .keyboardType(UIKeyboardType.emailAddress)
-                            .frame(maxWidth: 200)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                        
-                        Text("Password")
-                            .font(.title2)
-                            .foregroundColor(.white)
-                        
-                        SecureField(
-                            "Password", text: $password
-                        ) {
-                            print("Enter tapped on password field")
-                        }
-                        .frame(maxWidth: 200)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        
-                        Button(action: { print("tap button") }) {
-                            Text("Log in")
-                                .font(.title)
-                                .foregroundColor(.green)
-                            
-                        }
-                        .padding(.top, 30.0)
-                        .disabled(login.isEmpty || password.isEmpty)
-                        
-                        CellView(image: "face.smiling", firstLine: "Text you want 1", secondLine: "Text you want 2ds dsdfsdgsf")
-                    }
-                }
-                .frame(width: geometry.size.width, height: geometry.size.height, alignment: .center)
-                .onTapGesture {
-                    UIApplication.shared.hideKeyboard()
-                }
+        NavigationView {
+            HStack{
+                LoginView(presentMainScreen: $presentMainScreen)
+                NavigationLink(
+                    isActive: $presentMainScreen,
+                    destination: { EmptyView() },
+                    label: { EmptyView() }
+                )
             }
-        }.ignoresSafeArea(.keyboard, edges: .bottom)
-    }
-}
-
-
-
-//MARK: CellView
-struct CellView: View {
-    let image: String
-    let firstLine: String
-    let secondLine: String
-    
-    
-    var body: some View {
-        HStack(alignment: .center){
-            ImageCellView() {
-                Image(systemName: image)
-            }
-            VStack(alignment: .leading){
-                Text(firstLine)
-                    .modifier(TextCellViewModifier(font: .title3, color: .white))
-                Text(secondLine)
-                    .modifier(TextCellViewModifier(font: .body, color: .white))
-            }
-            Spacer()
         }
-        .frame(maxWidth: .infinity)
-        .background(.white.opacity(0.1))
     }
 }
-
-// MARK: Text ViewModifier
-struct TextCellViewModifier: ViewModifier {
-    let font: Font
-    let color: Color
-    
-    func body(content: Content) -> some View {
-            content
-            .font(font)
-            .foregroundColor(color)
-    }
-}
-
-// MARK: Image ViewBuilder
-struct ImageCellView: View {
-    var content: Image
-    
-    init(@ViewBuilder content: () -> Image) {
-        self.content = content()
-    }
-    
-    var body: some View {
-        content
-            .resizable()
-            .frame(width: 100.0, height: 100.0, alignment: .leading)
-            .padding(.leading, 10.0)
-    }
-}
-
-
-//MARK: hideKeyboard
-extension UIApplication {
-    func hideKeyboard(){
-        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-    }
-}
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .previewDevice("iPhone 12")
 .previewInterfaceOrientation(.portrait)
-//        ContentView()
-//            .previewDevice("iPad mini (6th generation)")
     }
 }
---
+++ 6_SwiftUI/VKGB_SwiftUI/VKGB_SwiftUI/LoginView.swift
@@ -0,0 +1,172 @@
+//
+//  LoginView.swift
+//  VKGB_SwiftUI
+//
+//  Created by Игорь Андрианов on 21.01.2022.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State private var login = ""
+    @State private var password = ""
+    @State private var isAlertPresented = false
+    @Binding private var presentMainScreen: Bool
+    
+    private let myLogin = "1"
+    private let myPassword = "1"
+    
+    init(presentMainScreen: Binding<Bool>){
+        self._presentMainScreen = presentMainScreen
+    }
+    
+    var body: some View {
+        GeometryReader{ geometry in
+            ZStack{
+                Image("back")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(width: geometry.size.width, height: geometry.size.height, alignment: .center)
+                
+                ScrollView{
+                    VStack{
+                        Text("VKBGClient")
+                            .padding(.top, 50.0)
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                        
+                        Text("Login")
+                            .padding(.top, 50.0)
+                            .font(.title2)
+                            .foregroundColor(.white)
+                        
+                        TextField(                            "Email address", text: $login)
+                            .textInputAutocapitalization(.never)
+                            .disableAutocorrection(true)
+                            .keyboardType(UIKeyboardType.emailAddress)
+                            .frame(maxWidth: 200)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        
+                        Text("Password")
+                            .font(.title2)
+                            .foregroundColor(.white)
+                        
+                        SecureField(
+                            "Password", text: $password
+                        ) {
+                            print("Enter tapped on password field")
+                        }
+                        .frame(maxWidth: 200)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        
+                        Button(action: { checkLoginPassword() }) {
+                            Text("Log in")
+                                .font(.title)
+                                .foregroundColor(.green)
+                            
+                        }
+                        .padding(.top, 30.0)
+                        .disabled(login.isEmpty || password.isEmpty)
+                        
+                        CellView(image: "face.smiling", firstLine: "Text you want 1", secondLine: "Text you want 2ds dsdfsdgsf")
+                    }
+                }
+                .frame(width: geometry.size.width, height: geometry.size.height, alignment: .center)
+                .onTapGesture {
+                    UIApplication.shared.hideKeyboard()
+                }
+            }
+        }
+        .ignoresSafeArea(.keyboard, edges: .bottom)
+        .alert(isPresented: $isAlertPresented) {
+            Alert(
+                title: Text("Ошибка"),
+                message: Text("Пара логин/пароль не верны. Логин и пароль это '1'"))
+        }
+    }
+    
+    //MARK: login func
+    private func checkLoginPassword() {
+        if myLogin == login, myPassword == password {
+            presentMainScreen = true
+        } else {
+            isAlertPresented = true
+        }
+    }
+    
+}
+
+
+
+
+//MARK: CellView
+struct CellView: View {
+    let image: String
+    let firstLine: String
+    let secondLine: String
+    
+    
+    var body: some View {
+        HStack(alignment: .center){
+            ImageCellView() {
+                Image(systemName: image)
+            }
+            VStack(alignment: .leading){
+                Text(firstLine)
+                    .modifier(TextCellViewModifier(font: .title3, color: .white))
+                Text(secondLine)
+                    .modifier(TextCellViewModifier(font: .body, color: .white))
+            }
+            Spacer()
+        }
+        .frame(maxWidth: .infinity)
+        .background(.white.opacity(0.1))
+    }
+}
+
+// MARK: Text ViewModifier
+struct TextCellViewModifier: ViewModifier {
+    let font: Font
+    let color: Color
+    
+    func body(content: Content) -> some View {
+            content
+            .font(font)
+            .foregroundColor(color)
+    }
+}
+
+// MARK: Image ViewBuilder
+struct ImageCellView: View {
+    var content: Image
+    
+    init(@ViewBuilder content: () -> Image) {
+        self.content = content()
+    }
+    
+    var body: some View {
+        content
+            .resizable()
+            .frame(width: 100.0, height: 100.0, alignment: .leading)
+            .padding(.leading, 10.0)
+    }
+}
+
+
+//MARK: hideKeyboard
+extension UIApplication {
+    func hideKeyboard(){
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView(presentMainScreen: .constant(false))
+            .previewDevice("iPhone 12")
+.previewInterfaceOrientation(.portrait)
+    }
+}
